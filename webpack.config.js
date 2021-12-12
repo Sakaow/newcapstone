33f10fd5064df1a6476423b99c2e42e3420c719e
Sakaowduan <billmichelle33@gmail.com>
2021-12-12 13:54:54 +1100

    use grid layout

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
+        // assetModuleFilename: "images/[name][ext]",
         assetModuleFilename: 'src/client/images/[name][ext]',
         clean: true,
     },
